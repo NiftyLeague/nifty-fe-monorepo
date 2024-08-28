af65c3ce98cad77fbf6f1aa8f269d8b3d9b3422e
NiftyAndy <andy@niftyleague.com>
2024-08-28 11:28:14 -0400

    Remove profile ID query param & disable blur event handler

--- apps/web/src/app/(special-routes)/invite/[game]/[refcode]/components/InviteRedirect.tsx
+++ apps/web/src/app/(special-routes)/invite/[game]/[refcode]/components/InviteRedirect.tsx
@@ -1,38 +1,35 @@
 'use client';
 
-import { useParams, useRouter, useSearchParams } from 'next/navigation';
+import { useParams, useRouter } from 'next/navigation';
 import Loading from './Loading';
 
 const isAndroid = (userAgent: string) => /.*(Mobile|Android).*/.test(userAgent);
 const isIOS = (userAgent: string) => /.*(iPhone|iPad|iPod).*/.test(userAgent);
 
-const redirectToAppStore = (userAgent: string, referral: string) => {
+const redirectToAppStore = (userAgent: string, refcode: string) => {
   let appStoreURL = 'https://niftysmashers.com';
   if (isAndroid(userAgent)) appStoreURL = `https://niftysmashers.com/android`;
   if (isIOS(userAgent)) appStoreURL = `https://niftysmashers.com/ios`;
-  window.location.href = `${appStoreURL}/?referral=${referral}`;
+  window.location.href = `${appStoreURL}/?referral=${refcode}`;
 };
 
 // Attempt to launch App if installed. Fallback to App Store after a timeout
-const redirectToNativeApp = (userAgent: string, profileId: string) => {
-  const timeoutId = setTimeout(() => redirectToAppStore(userAgent, profileId), 1500);
+const redirectToNativeApp = (userAgent: string, refcode: string) => {
+  let timeoutId = setTimeout(() => redirectToAppStore(userAgent, refcode), 500);
 
   const clearTimeoutHandler = () => {
     clearTimeout(timeoutId);
-    document.removeEventListener('visibilitychange', clearTimeoutHandler);
+    setTimeout(() => redirectToAppStore(userAgent, refcode), 10000);
     window.removeEventListener('beforeunload', clearTimeoutHandler);
-    window.removeEventListener('blur', clearTimeoutHandler);
-    window.removeEventListener('pagehide', clearTimeoutHandler);
+    // window.removeEventListener('blur', clearTimeoutHandler);
   };
 
-  // Clear the timeout if the page becomes hidden (app opens) or blurred (popup opens)
-  document.addEventListener('visibilitychange', clearTimeoutHandler);
+  // Clear the timeout if the page unloads (app opens) or blurred (popup opens)
   window.addEventListener('beforeunload', clearTimeoutHandler);
-  window.addEventListener('blur', clearTimeoutHandler);
-  window.addEventListener('pagehide', clearTimeoutHandler);
+  // window.addEventListener('blur', clearTimeoutHandler);
 
   // Attempt to launch the app
-  window.location.href = `nifty://niftysmashers/invite?profile=${profileId}`;
+  window.location.href = `nifty://niftysmashers/invite?profile=${refcode}`;
 };
 
 type RequestParams = { game: string; refcode: string };
@@ -40,20 +37,19 @@ type RequestParams = { game: string; refcode: string };
 const InviteRedirect = () => {
   const router = useRouter();
   const params = useParams();
-  const searchParams = useSearchParams();
 
   const { game, refcode } = params as RequestParams;
-  const profileId = searchParams.get('profile');
   const userAgent = navigator.userAgent;
 
   switch (game) {
     case 'smashers':
-      if (profileId && (isAndroid(userAgent) || isIOS(userAgent))) {
-        redirectToNativeApp(userAgent, profileId);
+      if (refcode.length > 7 && (isAndroid(userAgent) || isIOS(userAgent))) {
+        redirectToNativeApp(userAgent, refcode);
       } else {
         redirectToAppStore(userAgent, refcode);
       }
       break;
+    case 'royale':
     default:
       router.push('/');
   }
