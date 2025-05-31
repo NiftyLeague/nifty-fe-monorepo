fb86f2b8d1448928a20fba464fc1704c1e859af9
NiftyAndy <andy@niftyleague.com>
2025-05-31 17:47:59 -0400

    Cleanup Smashers site Metadata

--- apps/smashers/src/app/(auth_routes)/layout.tsx
+++ apps/smashers/src/app/(auth_routes)/layout.tsx
@@ -2,7 +2,17 @@ import { type PropsWithChildren, Suspense } from 'react';
 import { type Metadata } from 'next';
 import { AuthProvider } from '@/contexts/AuthProvider';
 
-export const metadata: Metadata = { title: 'Profile' };
+export const metadata: Metadata = {
+  title: 'Profile',
+  robots: {
+    index: false,
+    follow: true,
+    googleBot: {
+      index: false,
+      follow: true,
+    },
+  },
+};
 
 export default function AuthLayout({ children }: PropsWithChildren) {
   return (
