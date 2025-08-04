be9da110897f992cb1dd7f5beff7aa3aa31cdfa4
NiftyAndy <andy@niftyleague.com>
2025-08-04 14:25:20 -0400

    Git remove cache folders

--- packages/ui/src/components/custom/Loading/index.tsx
+++
@@ -1,7 +0,0 @@
-import { Preloader } from '../preloader/base';
-
-export function Loading({ ready = false }: { ready?: boolean }) {
-  return <Preloader ready={ready} />;
-}
-
-export default Loading;
