4ec493cf07e5f1d13cbf43a2f47975f073d17c1a
NiftyAndy <83057795+0xPlayerOne@users.noreply.github.com>
2026-08-10 22:42:06 -0400

    perf: defer shared Sentry client SDK (#474)
    Co-authored-by: intern <intern@niftyleague.com>

--- apps/web/src/instrumentation-client.ts
+++ apps/web/src/instrumentation-client.ts
@@ -2,22 +2,13 @@
 // The added config here will be used whenever a users loads a page in their browser.
 // https://docs.sentry.io/platforms/javascript/guides/nextjs/
 
-import * as Sentry from '@sentry/nextjs'
+import { captureRouterTransitionStart, scheduleSentryInit } from '@nl/sentry-client/client'
 
-if (process.env.VERCEL_ENV === 'production') {
-  Sentry.init({
-    dsn: 'https://97a944f1560b45018f013090ced577b3@o1377979.ingest.us.sentry.io/4504089815351296',
+scheduleSentryInit(process.env.VERCEL_ENV === 'production', {
+  dsn: 'https://97a944f1560b45018f013090ced577b3@o1377979.ingest.us.sentry.io/4504089815351296',
+  sendDefaultPii: true,
+  tracesSampleRate: 0.1,
+  debug: false,
+})
 
-    // Adds request headers and IP for users, for more info visit:
-    // https://docs.sentry.io/platforms/javascript/guides/nextjs/configuration/options/#sendDefaultPii
-    sendDefaultPii: true,
-
-    // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
-    tracesSampleRate: 0.1, // 10% trace sampling to limit client overhead
-
-    // Setting this option to true will print useful information to the console while you're setting up Sentry.
-    debug: false,
-  })
-}
-
-export const onRouterTransitionStart = Sentry.captureRouterTransitionStart
+export const onRouterTransitionStart = captureRouterTransitionStart
