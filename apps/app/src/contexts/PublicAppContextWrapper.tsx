d59148ee8277f4021a182dd4ea2897abc60c3b9b
intern <intern@niftyleague.com>
2026-08-10 21:47:35 -0400

    perf(app): defer public wallet storage

--- apps/app/src/contexts/PublicAppContextWrapper.tsx
+++ apps/app/src/contexts/PublicAppContextWrapper.tsx
@@ -3,15 +3,12 @@
 import type { PropsWithChildren } from 'react'
 
 import { FeatureFlagProvider } from '@/contexts/FeatureFlagsContext'
-import { LocalStorageProvider } from '@/contexts/LocalStorageContext'
 import ReduxProvider from '@/store/ReduxProvider'
 
 export default async function PublicAppContextWrapper({ children }: PropsWithChildren) {
   return (
-    <LocalStorageProvider>
-      <ReduxProvider>
-        <FeatureFlagProvider>{children}</FeatureFlagProvider>
-      </ReduxProvider>
-    </LocalStorageProvider>
+    <ReduxProvider>
+      <FeatureFlagProvider>{children}</FeatureFlagProvider>
+    </ReduxProvider>
   )
 }
