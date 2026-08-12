956916278d377d5a7ddf7fefeef772b185fda21f
NiftyAndy <83057795+0xPlayerOne@users.noreply.github.com>
2026-08-11 21:05:15 -0400

    perf(app): defer private network warning (#530)
    * perf(app): defer private network warning
    
    * ci(app): retrigger ready validation
    
    * fix(app): announce network warnings
    
    ---------
    
    Co-authored-by: intern <intern@niftyleague.com>

--- apps/app/src/app/_layout/_MainLayout/index.tsx
+++ apps/app/src/app/_layout/_MainLayout/index.tsx
@@ -2,54 +2,20 @@
 
 // third party
 import { type PropsWithChildren } from 'react'
-import { useAccount, useSwitchChain } from 'wagmi'
-import { immutableZkEvm, immutableZkEvmTestnet } from 'viem/chains'
+import dynamic from 'next/dynamic'
 
 // project imports
-import { Button } from '@nl/ui/base/button'
-import { TARGET_NETWORK } from '@/constants/networks'
-
-// components
-import { Icon } from '@nl/ui/base/icon'
 import AppShell from '@/app/_layout/AppShell'
 import Header from './_Header'
 import Sidebar from './_Sidebar'
 
+const NetworkWarning = dynamic(() => import('./_Header/NetworkWarning'), { ssr: false })
+
 // ==============================|| MAIN LAYOUT ||============================== //
 
 const MainLayout = ({ children }: PropsWithChildren) => {
-  const { address, chain } = useAccount()
-  const { switchChain } = useSwitchChain()
-  const isConnectedToIMX = chain?.id === immutableZkEvm.id || chain?.id === immutableZkEvmTestnet.id
-
-  const networkWarning =
-    address && TARGET_NETWORK.chainId !== chain?.id ? (
-      <div
-        className={
-          isConnectedToIMX
-            ? 'bg-success-dark/[80%] flex h-[60px] w-full items-center justify-center'
-            : 'bg-error/[80%] flex h-[60px] w-full items-center justify-center'
-        }
-        style={{ zIndex: 1, position: 'absolute' }}
-      >
-        <Icon name={isConnectedToIMX ? 'info' : 'triangle-alert'} size="lg" strokeWidth={2.5} />
-        <span className="px-2 text-xl font-semibold">
-          {isConnectedToIMX
-            ? `You're connected to Immutable zkEVM! Switch back to ${TARGET_NETWORK.label}`
-            : `Please switch to ${TARGET_NETWORK.label}`}
-        </span>
-        <Button
-          className="px-4 py-0.5"
-          variant="default"
-          onClick={() => switchChain?.({ chainId: TARGET_NETWORK.chainId })}
-        >
-          Switch
-        </Button>
-      </div>
-    ) : null
-
   return (
-    <AppShell header={<Header />} sidebar={<Sidebar />} networkWarning={networkWarning}>
+    <AppShell header={<Header />} sidebar={<Sidebar />} networkWarning={<NetworkWarning />}>
       {children}
     </AppShell>
   )
