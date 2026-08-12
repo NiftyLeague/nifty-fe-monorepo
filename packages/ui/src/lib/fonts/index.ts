5f4ac84266fc7874d60e146d55ae0230e5f57e49
NiftyAndy <83057795+0xPlayerOne@users.noreply.github.com>
2026-08-11 20:07:43 -0400

    perf(ui): scope font preloads by app theme (#533)
    Co-authored-by: intern <intern@niftyleague.com>

--- packages/ui/src/lib/fonts/index.ts
+++ packages/ui/src/lib/fonts/index.ts
@@ -1,40 +1,16 @@
-import localFont from 'next/font/local'
+export { defaultFont as imbPlexSans } from './default'
+export { headerFont as nexaRustSansBlack } from './header'
+export { specialFont as pressStart } from './special'
+export { subheaderFont as lilitaOne } from './subheader'
 
-export const imbPlexSans = localFont({
-  src: [
-    {
-      path: './assets/ibm-plex-sans-400.woff2',
-      weight: '400 700',
-      style: 'normal',
-    },
-    {
-      path: './assets/ibm-plex-sans-italic-400.woff2',
-      weight: '400 700',
-      style: 'italic',
-    },
-  ],
-  variable: '--font-ibm-plex-sans',
-  display: 'swap',
-})
+import { defaultFont } from './default'
+import { headerFont } from './header'
+import { specialFont } from './special'
+import { subheaderFont } from './subheader'
 
-export const lilitaOne = localFont({
-  src: './assets/lilita-one-400.woff2',
-  variable: '--font-lilita-one',
-  display: 'swap',
-})
-
-export const pressStart = localFont({
-  src: './assets/press-start-2p-400.woff2',
-  variable: '--font-press-start',
-  display: 'swap',
-})
-
-export const nexaRustSansBlack = localFont({
-  src: './NexaRustSans_Black/NexaRustSans-Black.woff2',
-  weight: '700',
-  style: 'normal',
-  variable: '--font-nexa-rust-sans-black',
-  display: 'swap',
-})
-
-export const customFontClassName = `${imbPlexSans.variable} ${lilitaOne.variable} ${nexaRustSansBlack.variable} ${pressStart.variable}`
+export const customFontClassName = [
+  defaultFont.variable,
+  subheaderFont.variable,
+  headerFont.variable,
+  specialFont.variable,
+].join(' ')
