9ec52776c8f0a3b482b0271884bf356ac6161f52
A. Mahoney-Fernandes <83057795+0xPlayerOne@users.noreply.github.com>
2026-08-26 13:55:28 -0400

    perf(docs): use native external social links

--- apps/docs/src/components/HomepageSocials/index.tsx
+++ apps/docs/src/components/HomepageSocials/index.tsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import Link from '@docusaurus/Link'
 import Discord from '@site/public/icons/socials/discord.svg'
 import Twitter from '@site/public/icons/socials/twitterX.svg'
 import Github from '@site/public/icons/socials/github.svg'
@@ -12,7 +11,7 @@ export default function HomepageSocials() {
   return (
     <Section>
       <RowThree>
-        <Link style={{ textDecoration: 'none' }} href={'https://discord.gg/niftyleague'}>
+        <a style={{ textDecoration: 'none' }} href="https://discord.gg/niftyleague">
           <CenterCard>
             <StyledIcon>
               <Discord style={{ width: '48px', height: '48px' }} />
@@ -22,8 +21,8 @@ export default function HomepageSocials() {
               <p>Join our community for realtime Q&amp;A.</p>
             </div>
           </CenterCard>
-        </Link>
-        <Link style={{ textDecoration: 'none' }} href={'https://twitter.com/NiftyLeague'}>
+        </a>
+        <a style={{ textDecoration: 'none' }} href="https://twitter.com/NiftyLeague">
           <CenterCard>
             <StyledIcon>
               <Twitter style={{ width: '48px', height: '48px' }} />
@@ -33,9 +32,9 @@ export default function HomepageSocials() {
               <p>Follow our latest annoucements.</p>
             </div>
           </CenterCard>
-        </Link>
+        </a>
 
-        <Link style={{ textDecoration: 'none' }} href={'https://github.com/NiftyLeague'}>
+        <a style={{ textDecoration: 'none' }} href="https://github.com/NiftyLeague">
           <CenterCard>
             <StyledIcon>
               <Github style={{ width: '48px', height: '48px' }} />
@@ -45,17 +44,17 @@ export default function HomepageSocials() {
               <p>View all public Nifty League repositories.</p>
             </div>
           </CenterCard>
-        </Link>
+        </a>
       </RowThree>
       <Row>
-        <Link
+        <a
           style={{
             textDecoration: 'none',
             maxWidth: '960px',
             margin: '0 auto 4rem auto',
             width: '100%',
           }}
-          href={'https://www.twitch.tv/NiftyLeagueOfficial'}
+          href="https://www.twitch.tv/NiftyLeagueOfficial"
         >
           <WideCard>
             <img
@@ -75,7 +74,7 @@ export default function HomepageSocials() {
               </p>
             </div>
           </WideCard>
-        </Link>
+        </a>
       </Row>
     </Section>
   )
