159ab4d3867198e54c79cdfbbd7d06048f027b78
A. Mahoney-Fernandes <83057795+0xPlayerOne@users.noreply.github.com>
2026-08-26 13:26:40 -0400

    chore(web): remove unused responsive image import

--- apps/web/src/app/(main)/page.tsx
+++ apps/web/src/app/(main)/page.tsx
@@ -3,7 +3,7 @@ import { preload } from 'react-dom'
 import { DeferredConsoleGame } from '@nl/ui/custom/deferred-console-game'
 import { ConsoleGameBackdrop } from '@nl/ui/custom/console-game-backdrop'
 import OptimizedImage, { getOptimizedImageProps } from '@nl/ui/custom/optimized-image'
-import { DesktopOnlyImage, MobileOnlyImage } from '@nl/ui/custom/responsive-only-image'
+import { DesktopOnlyImage } from '@nl/ui/custom/responsive-only-image'
 
 import {
   DeferredHomeCommunity,
