d7e4efd496bc8110e9afe6b860c6c94247ad2a07
A. Mahoney-Fernandes <83057795+0xPlayerOne@users.noreply.github.com>
2026-09-06 19:29:29 -0400

    fix: restore DeferredSkeleton usages broken by rename hybrid

--- apps/app/src/components/providers/PrivateRoutesShell.tsx
+++ apps/app/src/components/providers/PrivateRoutesShell.tsx
@@ -39,8 +39,8 @@ function PrivateRoutesContentLoading(): React.ReactNode {
       aria-busy="true"
     >
       <div className="flex min-h-0 flex-1 gap-6">
-        <Skeleton className="hidden w-64 rounded-lg lg:block" />
-        <Skeleton className="min-h-[20rem] flex-1 rounded-lg" />
+        <DeferredSkeleton className="hidden w-64 rounded-lg lg:block" />
+        <DeferredSkeleton className="min-h-[20rem] flex-1 rounded-lg" />
       </div>
       <span className="sr-only">Loading private app content</span>
     </div>
@@ -56,8 +56,8 @@ function PrivateRoutesContentLoading(): React.ReactNode {
       aria-busy="true"
     >
       <div className="flex min-h-0 flex-1 gap-6">
-        <Skeleton className="hidden w-64 rounded-lg lg:block" />
-        <Skeleton className="min-h-[20rem] flex-1 rounded-lg" />
+        <DeferredSkeleton className="hidden w-64 rounded-lg lg:block" />
+        <DeferredSkeleton className="min-h-[20rem] flex-1 rounded-lg" />
       </div>
       <span className="sr-only">Loading private app content</span>
     </div>
